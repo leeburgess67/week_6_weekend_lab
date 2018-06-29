522d413387b14dd54879a9b608a71917958744ad
leeburgess67 <leeburgessmail@gmail.com>
2018-06-29 14:42:43 +0100

    Hotel class set up

--- Weekend Homework Hotel/src/test/java/HotelTest.java
+++ Weekend Homework Hotel/src/test/java/HotelTest.java
@@ -1,2 +1,36 @@
+import Hotel.AllRooms.*;
+import org.junit.Before;
+
+import java.util.ArrayList;
+
 public class HotelTest {
+
+    Hotel hotel;
+    Bedroom bedroom1;
+    Bedroom bedroom2;
+    DiningRoom diningRoom1;
+    ConferenceRoom conferenceRoom1;
+    ConferenceRoom conferenceRoom2;
+    ArrayList<Room> bedrooms;
+    ArrayList<Room> diningRooms;
+    ArrayList<Room> conferenceRooms;
+
+
+
+    @Before
+    public void before(){
+     bedrooms = new ArrayList<>();
+     diningRooms = new ArrayList<>();
+     conferenceRooms = new ArrayList<>();
+
+     bedrooms.add(bedroom1);
+     bedrooms.add(bedroom2);
+     diningRooms.add(diningRoom1);
+     conferenceRooms.add(conferenceRoom1);
+     conferenceRooms.add(conferenceRoom2);
+
+     hotel = new Hotel(bedrooms, diningRooms, conferenceRooms);
+    }
+
+
 }
